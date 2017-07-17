59c4b2619068e1f173d71db30288edd6b9e214aa
chen <chenshaobo272@gmail.com>
2017-07-17 23:39:11 +0800

    two file

---
+++ kuaisu.cpp
@@ -0,0 +1,35 @@
+#include<iostream>                    
+using namespace std;
+void swap(int &a,int &b)       
+{
+	int tmp=a;
+	a=b;
+	b=tmp;
+}
+void QuickSort(int a[],int s,int e)  
+{
+	if(s>=e)
+		return;                       
+	int k=a[s];                       
+	int i=s,j=e;                      
+	while(i!=j){
+		while(i<j&&a[j]>=k)           
+			--j;	
+		swap(a[i],a[j]);
+		while(i<j&&a[i]<=k)
+			++i;
+		swap(a[i],a[j]);
+	}                                
+	QuickSort(a,s,i-1);
+	QuickSort(a,i+1,e);
+}
+int a[]={93,27,30,2,8,12,2,8,30,89,55,78};
+int main()
+{
+	int size=sizeof(a)/4;           
+	QuickSort(a,0,size-1);
+	for(int i=0;i<size;++i)
+		cout<<a[i]<<",";
+	cout<<endl;
+	return 0;
+}
