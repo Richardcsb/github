0c556fce5c795c9659d7726f8aa4cadb15b0fb67
chen <chenshaobo272@gmail.com>
2017-07-20 18:19:58 +0800

    rm cpp

--- procedure/CPP/test.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	char s[20];
-	char a;
-	a=getc();
-	printf("%c",a);
-	return 0;
-}
--- procedure/CPP/test1.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	char c;
-	for(;c=getchar()!='\n';){
-		printf("%c,%d\n",c,c);
-	}
-	return 0;
-}
